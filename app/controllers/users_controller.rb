9288536f986d516fcb666ad3bde313425c823333
Chris Tzitzas <ctzitzas@gmail.com>
2021-07-18 21:44:51 +1000

    CONTROLLER: Added login action. CONTROLLER: Added authentication and JWT encoding to application controller. CREDENTIALS: Added jwt secret key.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,7 @@
 class UsersController < ApplicationController
 
-  before_action :set_user, only: [:show] 
-
-  def index
-    @users = User.all
-    render json: @users
-  end
+  before_action :set_user, only: [:show]
+  before_action :authenticated, only: [:show]
 
   def show
     render json: @user
@@ -21,8 +17,12 @@ class UsersController < ApplicationController
   end
 
   def login
-    @user = User.find_by_id(params[:id])
+    @user = User.find_by_email(params[:email])
     if @user && @user.authenticate(params[:password])
+      render json: {message: 'Sucessful login', token: encode({user_id: @user.id})}, status: :ok
+    else
+      render json: {error: "Invalid username or password"}
+    end
   end
 
   private
