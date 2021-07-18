b2fbb00b0de3a4bf70ea06e3ee54b60efddd6090
Chris Tzitzas <ctzitzas@gmail.com>
2021-07-18 15:43:44 +1000

    MODEL: removed validation made redundant by password regex

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,6 @@ class User < ApplicationRecord
   belongs_to :store
   validates :name, :email, :password_digest, :key, :user_type, presence: true
   validates :email, :key, uniqueness: true
-  validates :password_digest, length: { minimum: 8 }
   validates_format_of :password_digest, with: password_format
   validates_format_of :email, with: URI::MailTo::EMAIL_REGEXP
   enum user_type: { user: 1, admin: 2 }
