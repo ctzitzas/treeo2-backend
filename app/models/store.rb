32e09511267dc2802d6b309075f6d80ec1b88d6f
Chris Tzitzas <ctzitzas@gmail.com>
2021-07-21 12:33:03 +1000

    MODELS: Added key column to store model.

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,6 +1,6 @@
 class Store < ApplicationRecord
   has_many :users
-  validates :name, presence: true, uniqueness: true
+  validates :name, :key, presence: true, uniqueness: true
   validates :address, :suburb, :postcode, presence: true
   validates :state, presence: true, length: { minimum: 2 }
 end
