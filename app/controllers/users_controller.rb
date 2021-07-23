e52d8832fb7fe6eb832f02c58745f202e2d8001e
Chris Tzitzas <ctzitzas@gmail.com>
2021-07-23 15:02:05 +1000

    Modified seed data and models to allow predetermined keys

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
 
   before_action :set_user, only: [:show, :login]
-  before_action :set_response, only: [:sign_up, :login]
   before_action :authenticated, only: [:show]
 
   def show
@@ -33,28 +32,28 @@ class UsersController < ApplicationController
     rescue
       render json: {error: "Page not found"}, status: 404
     end
+    begin
+      @store = Store.find_by(id: @user.store_id)
+      @login_response = {
+        name: @user.name,
+        email: @user.email,
+        key: @user.key,
+        token: encode({user_id: @user.id}),
+        store: {
+          name: @store.name,
+          address: @store.address,
+          suburb: @store.suburb,
+          state: @store.state,
+          postcode: @store.postcode,
+          key: @store.key
+        }
+        }
+    rescue 
+      render json: {error: 'set_response failed'}, status: 404
+    end
   end
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation, :key, :user_type, :store_id)
   end
-
-  def set_response
-    @store = Store.find_by(id: @user.store_id)
-    @login_response = {
-      name: @user.name,
-      email: @user.email,
-      key: @user.key,
-      token: encode({user_id: @user.id}),
-      store: {
-        name: @store.name,
-        address: @store.address,
-        suburb: @store.suburb,
-        state: @store.state,
-        postcode: @store.postcode,
-        key: @store.key
-      }
-      }
-  end
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,13 +9,16 @@ class User < ApplicationRecord
   validates_format_of :email, with: URI::MailTo::EMAIL_REGEXP
   enum user_type: { user: 1, admin: 2 }
   attribute :user_type, :integer, default: 1
+  attribute :store_id, :integer, default: 1
 
   private
 
   def randomise_key
-    begin
-      self.key = SecureRandom.alphanumeric(12)
-    end while self.class.where(key: self.key).exists?
+    if !self.key
+      begin
+        self.key = SecureRandom.alphanumeric(12)
+      end while self.class.where(key: self.key).exists?
+    end
   end
 end
 
