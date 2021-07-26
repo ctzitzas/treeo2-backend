a128a69b324d0f41b72f39f53171f7e8d17d2896
Chris Tzitzas <ctzitzas@gmail.com>
2021-07-26 14:35:12 +1000

    STRIPE: Created webhook for order fulfillment

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,11 +1,8 @@
 class PurchasesController < ApplicationController
 
-  before_action :payment_params
+  before_action :payment_params, only: [:create]
 
   def create
-
-    url = 'http://localhost:3001'
-
       session = Stripe::Checkout::Session.create({
         payment_method_types: ['card'],
         line_items: [{
@@ -19,13 +16,41 @@ class PurchasesController < ApplicationController
           quantity: params[:quantity]
         }],
         mode: 'payment',
-        success_url: url + '/AdoptedTree',
-        cancel_url: url + '/Adopt'
+        success_url: 'http://localhost:3001/Mytrees',
+        cancel_url: 'http://localhost:3001/Adopt'
       })
       redirect_to session.url, status: 303
   end
 
+  def webhook
+    event = nil
+    begin
+      sig_header = request.env['HTTP_STRIPE_SIGNATURE']
+      payload = request.body.read
+      endpoint_secret = 'whsec_ODKTSUVbiSMMMPpq3VvNfiBjwUvIII50'
+      event = Stripe::Webhook.construct_event(payload, sig_header, endpoint_secret)
+    rescue JSON::ParserError => e
+      return head :bad_request
+    rescue Stripe::SignatureVerificationError => e
+      return head :bad_request
+    end
+
+    if event['type'] == 'checkout.session.completed'
+      checkout_session = event['data']['object']
+      fulfill_order(checkout-session)
+    end
+
+    head :ok
+  end
+
+  private
+
   def payment_params
     params.permit(:quantity)
+  end
+
+  def fulfill_order(checkout_session)
+    # TODO: fill in with your own logic
+    puts "Fulfilling order for #{checkout_session.inspect}"
   end
 end
